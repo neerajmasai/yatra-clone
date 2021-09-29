25079fdaed6c1d1d21ec8dd11f166c8eef7eec20
Shariq Ansari <ansarisharique0003@gmail.com>
2021-09-30 01:03:33 +0530

    Homepage and Navbar first push

---
+++ yatra/src/Components/Homepage/Homepage.jsx
@@ -0,0 +1,13 @@
+import { Navbar } from "../Header/Navbar"
+import {LeftSection} from '../LeftSection/LeftSection'
+const Homepage =()=>{
+    return <div>
+        <div> 
+        <Navbar />
+        </div>
+        
+        <LeftSection />
+    </div>
+}
+
+export{Homepage}
