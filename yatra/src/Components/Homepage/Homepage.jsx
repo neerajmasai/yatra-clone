d873cc3eed5f1d7e1d80e015f80481467543b995
Shariq Ansari <ansarisharique0003@gmail.com>
2021-09-30 01:05:17 +0530

    all changes

--- yatra/src/Components/Homepage/Homepage.jsx
+++ yatra/src/Components/Homepage/Homepage.jsx
@@ -1,12 +1,12 @@
 import { Navbar } from "../Header/Navbar"
-import {LeftSection} from '../LeftSection/LeftSection'
+
 const Homepage =()=>{
     return <div>
         <div> 
         <Navbar />
         </div>
         
-        <LeftSection />
+        
     </div>
 }
 
