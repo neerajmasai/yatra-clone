bf89360ea7a5cfc3880b95960d44f0cae98c9106
SunnyDhanwani <dhanwanisunny678@gmail.com>
2021-10-01 21:24:25 +0530

    Signup page

--- yatra/src/Routes/Router.jsx
+++ yatra/src/Routes/Router.jsx
@@ -1,17 +1,21 @@
 import { Route } from "react-router"
 import App from "../App";
 import { Booking } from "../Components/Booking-page/Booking";
+import { SignUp } from "../Components/SignUp-page/SignUp";
 
 
 export const Routes = ()=>{
-    return(
+    return (
         <>
-        <Route exact path="/">
-        <App />
-        </Route>
-        <Route path="/booking"> 
-        <Booking/>
-        </Route>
+            <Route exact path="/">
+                <App />
+            </Route>
+            <Route path="/booking">
+                <Booking />
+            </Route>
+            <Route path="/signup">
+                <SignUp />
+            </Route>
         </>
-    )
+    );
 }
